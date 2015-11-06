51176f575aac8ecf10f2aa98a594e3df7a309f51
Arturo <a245329@uach.mx>
2015-11-05 22:24:56 -0700

    Creando un área para editores personalizada

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -20,6 +20,9 @@ import models.*;
 @With(Secure.class)
 public class Admin extends Controller {
 
+    /**
+     * Interceptor para establecer los datos del usuario
+     */
     @Before
     static void setConnectedUser() {
         if (Security.isConnected()) {
@@ -28,8 +31,82 @@ public class Admin extends Controller {
         }
     }
 
+    /**
+     * Aquí usamos la instrucción render("@form") que viene a ser una versión
+     * simplificada de la instrucción render("Admin/form.html"). Esta
+     * instrucción simplemente le dice a Play que utilice la plantilla por
+     * defecto de la acción ‘form’.
+     *
+     * @param title
+     * @param content
+     * @param tags
+     */
+    public static void save(String title, String content, String tags) {
+        // Create post
+        User author = User.find("byEmail", Security.connected()).first();
+        Post post = new Post(author, title, content);
+        // Set tags list
+        for (String tag : tags.split("\\s+")) {
+            if (tag.trim().length() > 0) {
+                post.tags.add(Tag.findOrCreateByName(tag));
+            }
+        }
+        // Validate
+        validation.valid(post);
+        if (validation.hasErrors()) {
+            render("@form", post);
+        }
+        // Save
+        post.save();
+        index();
+    }
+
     public static void index() {
+        String user = Security.connected();
+        List<Post> posts = Post.find("author.email", user).fetch();
+        render(posts);
+    }
+
+    /**
+     * Primero necesitamos que Admin.form pueda además obtener los datos de un Post existente:
+     * @param id 
+     */
+    public static void form(Long id) {
+        if(id != null) {
+            Post post = Post.findById(id);
+            render(post);
+        }
         render();
+    }
+
+    public static void save(Long id, String title, String content, String tags) {
+        Post post;
+        if(id == null) {
+            // Create post
+            User author = User.find("byEmail", Security.connected()).first();
+            post = new Post(author, title, content);
+        } else {
+            // Retrieve post
+            post = Post.findById(id);
+            // Edit
+            post.title = title;
+            post.content = content;
+            post.tags.clear();
+        }
+        // Set tags list
+        for(String tag : tags.split("\\s+")) {
+            if(tag.trim().length() > 0) {
+                post.tags.add(Tag.findOrCreateByName(tag));
+            }
+        }
+        // Validate
+        validation.valid(post);
+        if(validation.hasErrors()) {
+            render("@form", post);
+        }
+        // Save
+        post.save();
+        index();
     }
 
 }
